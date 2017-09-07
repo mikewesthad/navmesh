7594559da0197a0fef23166c38f23175e5f6fd0d
Michael Hadley <michaelwesthadley@gmail.com>
2017-09-07 07:41:22 -0500

    Prettier + precommit hook

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,23 @@
 module.exports = {
-	// Enable core eslint rules, see: http://eslint.org/docs/rules/
-	extends: "eslint:recommended",
-	// Additional rules
-	rules: {
-		"brace-style": ["warn", "1tbs"],
-		"indent": ["warn", 4, {"SwitchCase": 1}],
-		"max-len": ["warn", 100, {"ignoreUrls": true}],
-		"no-mixed-spaces-and-tabs": "warn",
-		"no-console": "off"
-	},
-	env: {
-		browser: true,
-		commonjs: true,
-		es6: true
-	},
-	parserOptions: {
-		sourceType: "module"
-	},
-	globals: {
-		Phaser: true
-	}
-}
+  extends: ["eslint:recommended", "prettier"],
+  plugins: ["prettier"],
+  rules: {
+    "no-var": 1,
+    "brace-style": ["warn", "1tbs"],
+    "no-unused-vars": ["error", { args: "after-used" }],
+    indent: ["warn", 2, { SwitchCase: 1 }],
+    "max-len": ["warn", 100, { ignoreUrls: true, ignoreTemplateLiterals: true }],
+    "no-console": "off"
+  },
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true
+  },
+  parserOptions: {
+    sourceType: "module"
+  },
+  globals: {
+    Phaser: true
+  }
+};
