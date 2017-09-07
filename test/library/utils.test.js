a6774aa83915c9dcb254b034423a516ee5dfae4f
Michael Hadley <michaelwesthadley@gmail.com>
2017-09-07 08:51:31 -0500

    Adding unit tests

---
+++ test/library/utils.test.js
@@ -0,0 +1,38 @@
+const path = require("path");
+import { triarea2, almostEqual, angleDifference, areCollinear } from "../../src/library/utils";
+
+describe("almostEqual", () => {
+  test("should be false for numbers with a significant difference", () => {
+    expect(almostEqual(1, 2)).toEqual(false);
+    expect(almostEqual(-1, -0.95)).toEqual(false);
+  });
+  test("should be true for numbers that are within floating point error margin", () => {
+    expect(almostEqual(1, 1.00001)).toEqual(true);
+    expect(almostEqual(1 / 3, 0.33333)).toEqual(true);
+  });
+});
+
+describe("areCollinear", () => {
+  const line = (x1, y1, x2, y2) => ({ start: { x: x1, y: y1 }, end: { x: x2, y: y2 } });
+  test("should return false for non-collinear lines", () => {
+    expect(areCollinear(line(-5, 0, 5, 0), line(-5, 10, 5, 10))).toEqual(false); // Parallel
+    expect(areCollinear(line(0, 0, 10, 10), line(0, 0, 0, 10))).toEqual(false); // Intersecting
+  });
+  test("should return true for collinear lines", () => {
+    expect(areCollinear(line(10, 10, 0, 0), line(10, 10, 0, 0))).toEqual(true); // Same
+    expect(areCollinear(line(0, 0, 10, 10), line(10, 10, 0, 0))).toEqual(true); // Reversed
+  });
+});
+
+describe("angleDifference", () => {
+  test("should return the difference between angles in radians", () => {
+    expect(angleDifference(Math.PI, Math.PI / 2)).toEqual(Math.PI / 2);
+    expect(angleDifference(1, -2)).toEqual(3);
+  });
+  test("should wrap angles to calculate minimum angular distance", () => {
+    expect(angleDifference(4 * Math.PI, Math.PI / 2)).toEqual(-Math.PI / 2);
+    expect(angleDifference(-3 * Math.PI, 0)).toEqual(-Math.PI);
+  });
+});
+
+// TODO: triarea2
