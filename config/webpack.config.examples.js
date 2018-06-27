20ceddb55167419a548dbce7616902bd720c34e5
Michael Hadley <michaelwesthadley@gmail.com>
2018-06-26 21:07:12 -0500

    Update config for webpack 4 and phaser 3

--- config/webpack.config.examples.js
+++ config/webpack.config.examples.js
@@ -2,63 +2,43 @@
 
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const path = require("path");
+const webpack = require("webpack");
 
 // Go up one directory since we are in config/
 const root = path.join(__dirname, "..");
 
-module.exports = {
-  context: path.join(root, "src", "examples"),
-  entry: {
-    "demo/js/main": "./demo/js/main.js",
-    "performance/js/main": "./performance/js/main.js"
-  },
-  output: {
-    filename: "[name].js",
-    path: path.resolve(root, "public")
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ["babel-loader"]
-      },
-      {
-        test: require.resolve("phaser-ce/build/custom/pixi"),
-        use: [
-          {
-            loader: "expose-loader",
-            options: "PIXI"
-          }
-        ]
-      },
-      {
-        test: require.resolve("phaser-ce/build/custom/p2"),
-        use: [
-          {
-            loader: "expose-loader",
-            options: "p2"
-          }
-        ]
-      },
-      {
-        test: require.resolve("phaser-ce/build/custom/phaser-split"),
-        use: [
-          {
-            loader: "expose-loader",
-            options: "Phaser"
-          }
-        ]
-      }
-    ]
-  },
-  resolve: {
-    alias: {
-      pixi: require.resolve("phaser-ce/build/custom/pixi"),
-      p2: require.resolve("phaser-ce/build/custom/p2"),
-      phaser: require.resolve("phaser-ce/build/custom/phaser-split")
-    }
-  },
-  plugins: [new CopyWebpackPlugin([{ from: "**/*" }], { ignore: ["**/js/**/*"] })],
-  devtool: "source-map"
+module.exports = function(env, argv) {
+  const isDev = argv.mode === "development";
+
+  return {
+    mode: isDev ? "development" : "production",
+    context: path.join(root, "src", "examples"),
+    entry: {
+      "demo/js/main": "./demo/js/main.js",
+      "performance/js/main": "./performance/js/main.js"
+    },
+    output: {
+      filename: "[name].js",
+      path: path.resolve(root, "public")
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: "babel-loader"
+        }
+      ]
+    },
+    plugins: [
+      new CopyWebpackPlugin([{ from: "**/*" }], { ignore: ["**/js/**/*"] }),
+
+      new webpack.DefinePlugin({
+        "typeof CANVAS_RENDERER": JSON.stringify(true),
+        "typeof WEBGL_RENDERER": JSON.stringify(true),
+        PRODUCTION: !isDev
+      })
+    ],
+    devtool: isDev ? "eval-source-map" : "source-map"
+  };
 };
--- config/webpack.config.library.js
+++ config/webpack.config.library.js
@@ -6,33 +6,37 @@ const webpack = require("webpack");
 // Go up one directory since we are in config/
 const root = path.join(__dirname, "..");
 
-module.exports = {
-  context: path.join(root, "src", "library"),
-  entry: {
-    "phaser-navmesh": "./index.js",
-    "phaser-navmesh.min": "./index.js"
-  },
-  output: {
-    filename: "[name].js",
-    path: path.resolve(root, "dist"),
-    library: "PhaserNavmesh",
-    libraryTarget: "umd"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ["babel-loader"]
-      }
-    ]
-  },
-  plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      include: /\.min\.js$/,
-      minimize: true,
-      sourceMap: "source-map"
-    })
-  ],
-  devtool: "source-map"
+module.exports = function(env, argv) {
+  const isDev = argv.mode === "development";
+
+  return {
+    context: path.join(root, "src", "library"),
+    entry: {
+      "phaser-navmesh": "./index.js",
+      "phaser-navmesh.min": "./index.js"
+    },
+    output: {
+      filename: "[name].js",
+      path: path.resolve(root, "dist"),
+      library: "PhaserNavmesh",
+      libraryTarget: "umd"
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: ["babel-loader"]
+        }
+      ]
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        "typeof CANVAS_RENDERER": JSON.stringify(true),
+        "typeof WEBGL_RENDERER": JSON.stringify(true),
+        PRODUCTION: !isDev
+      })
+    ],
+    devtool: isDev ? "eval-source-map" : "source-map"
+  };
 };
