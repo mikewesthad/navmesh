cacf8f9356299cc7bd638f0afe63822914fd34a3
Michael Hadley <michaelwesthadley@gmail.com>
2018-06-26 21:12:00 -0500

    Update NavMeshPlugin for v3

--- src/library/nav-mesh-plugin.js
+++ src/library/nav-mesh-plugin.js
@@ -1,115 +1,144 @@
-// const triangulate = require("./triangulate");
+import Phaser from "phaser";
 import NavMesh from "./nav-mesh";
 
 /**
- * This plugin can create navigation meshes for use in Phaser. The navmeshes can be constructed from
- * convex polygons embedded in a Tiled map. Instantiate this using game.plugins.add(NavMeshPlugin).
+ * This class can create navigation meshes for use in Phaser. The navmeshes can be constructed from
+ * convex polygons embedded in a Tiled map. The class that conforms to Phaser's plugin structure,
+ * but doesn't explicitly require Phaser.
  *
- * @param {Phaser.Game} game
- * @param {Phaser.PluginManager} manager
+ * @export
+ * @class NavMeshPlugin
  */
-export default class NavMeshPlugin extends Phaser.Plugin {
-  constructor(game, manager) {
-    super(game, manager);
-    this._navMeshes = [];
+export default class NavMeshPlugin {
+  constructor() {
+    this.navMeshes = [];
   }
 
   /**
    * Load a navmesh from Tiled and switch it to be the current navmesh. Currently assumes that the
    * polygons are squares!
-   * 
-   * @param {Phaser.Tilemap} tilemap The tilemap that contains polygons under an object layer
-   * @param {string} objectKey The name of the object layer in the tilemap
-   * @param {number} [meshShrinkAmount=0] The amount (in pixels) that the navmesh has been
-   * shrunk around obstacles (a.k.a the amount obstacles have been expanded)
-   * 
+   *
+   * @param {Phaser.Tilemaps.Tilemap} tilemap The tilemap that contains polygons under an object
+   * layer
+   * @param {string} layerName The name of the object layer in the tilemap
+   * @param {number} [meshShrinkAmount=0] The amount (in pixels) that the navmesh has been shrunk
+   * around obstacles (a.k.a the amount obstacles have been expanded)
+   *
    * @memberof NavMeshPlugin
    */
-  buildMeshFromTiled(tilemap, objectKey, meshShrinkAmount = 0) {
-    if (!tilemap.objects[objectKey]) {
+  buildMeshFromTiled(tilemap, layerName, meshShrinkAmount = 0) {
+    const objectLayer = tilemap.getObjectLayer(layerName);
+    if (!objectLayer) {
       console.warn(
-        `NavMeshPlugin: The given tilemap has no object layer with the name "${objectKey}"`
+        `NavMeshPlugin: The given tilemap has no object layer with the name "${layerName}"`
       );
-    } else if (tilemap.objects[objectKey].length === 0) {
+    } else if (objectLayer.length === 0) {
       console.warn(
-        `NavMeshPlugin: The "${objectKey}" object layer in the Tilemap has 0 objects in it`
+        `NavMeshPlugin: The "${layerName}" object layer in the Tilemap has 0 objects in it`
       );
     }
+
     // Load up the object layer
-    const rects = tilemap.objects[objectKey] || [];
-    // Loop over the objects and construct a polygon
-    const polygons = [];
-    for (const r of rects) {
-      const top = r.y;
-      const bottom = r.y + r.height;
-      const left = r.x;
-      const right = r.x + r.width;
-      const poly = new Phaser.Polygon(left, top, left, bottom, right, bottom, right, top);
-      polygons.push(poly);
-    }
+    const objects = objectLayer ? objectLayer.objects : [];
+
+    // Loop over the objects and construct a polygon - assumes a rectangle for now!
+    // TODO: support layer position, scale, rotation
+    const polygons = objects.map(obj => {
+      const top = obj.y;
+      const bottom = obj.y + obj.height;
+      const left = obj.x;
+      const right = obj.x + obj.width;
+      return new Phaser.Geom.Polygon([[left, top], [left, bottom], [right, bottom], [right, top]]);
+    });
+
     // Build the navmesh
-    const mesh = new NavMesh(this.game, polygons, meshShrinkAmount);
-    this._navMeshes.push(mesh);
+    const mesh = new NavMesh(polygons, meshShrinkAmount);
+    this.navMeshes.push(mesh);
     return mesh;
   }
 
-  destroy() {
-    for (const mesh of this._navMeshes) mesh.destroy();
-    this._navMeshes = [];
-    super.destroy();
+  /**
+   * Register the plugin with the Phaser.Scene as "NavMeshPlugin" and make it accessible as
+   * `this.navMesh` and `this.sys.navMesh` under the scene.
+   *
+   * @static
+   * @param {Phaser.Plugins.PluginManager} pluginManager
+   * @memberof NavMeshPlugin
+   */
+  static register(pluginManager) {
+    pluginManager.register("NavMeshPlugin", NavMeshPlugin, "navMesh");
   }
 
-  // /**
-  //  * Build a navmesh from an array of convex polygons. This currently tesselates the polygons into
-  //  * triangles. They aren't as efficient or as well designed as ones made by hand in Tiled.
-  //  *
-  //  * @param {string} levelName The key to use to store the navmesh in the plugin
-  //  * @param {[]} hulls An array of convex polygons describing the obstacles in the
-  //  * level. See lighting-plugin/hull-from-tiles.
-  //  *
-  //  * @memberof NavMeshPlugin
-  //  */
-  // buildMesh(levelName, hulls) {
-  //     const contours = this._buildContours(hulls);
-  //     // Get an array of triangulated vertices
-  //     const triangles = triangulate(contours, false); // Counter-clockwise ordering!
-  //     const polygons = [];
-  //     for (let i = 0; i < triangles.length; i += 6) {
-  //         const poly = new Phaser.Polygon(
-  //             // These should be in counter-clockwise order from triangulate
-  //             triangles[i + 0], triangles[i + 1],
-  //             triangles[i + 2], triangles[i + 3],
-  //             triangles[i + 4], triangles[i + 5]
-  //         );
-  //         polygons.push(poly);
-  //     }
-  //     const navMesh = new NavMesh(this.game, polygons);
-  //     this._navMeshes[levelName] = navMesh;
-  //     this._currentNavMesh = navMesh;
-  // }
+  // TODO: hook into this for destroying
+  boot() {
+    // const eventEmitter = this.systems.events;
+    // eventEmitter.on("destroy", this.destroy, this);
+  }
 
-  // /**
-  //  * @param {[]} hulls
-  //  * @returns
-  //  *
-  //  * @memberof NavMeshPlugin
-  //  */
-  // _buildContours(hulls) {
-  //     const w = this.game.width;
-  //     const h = this.game.height;
-  //     // Start the contours
-  //     const contours = [
-  //         // Full screen - counter clockwise
-  //         Float32Array.of(0,0, 0,h, w,h, w,0)
-  //     ];
-  //     // For each convex hull add the contour
-  //     for (const hull of hulls) {
-  //         const contour = [];
-  //         for (const lineInfo of hull) {
-  //             contour.push(lineInfo.line.start.x, lineInfo.line.start.y);
-  //         }
-  //         contours.push(Float32Array.from(contour));
-  //     }
-  //     return contours;
-  // }
+  // Required by the Phaser PluginManager, but noop in this plugin
+  init() {}
+  start() {}
+
+  shutdown() {}
+  destroy() {
+    this.navMeshes.forEach(mesh => mesh.destroy());
+    this.navMeshes = [];
+  }
 }
+
+// TODO: integrate old code that automatically decomposed maps via triangulation
+// // const triangulate = require("./triangulate");
+//   // /**
+//   //  * Build a navmesh from an array of convex polygons. This currently tesselates the polygons into
+//   //  * triangles. They aren't as efficient or as well designed as ones made by hand in Tiled.
+//   //  *
+//   //  * @param {string} levelName The key to use to store the navmesh in the plugin
+//   //  * @param {[]} hulls An array of convex polygons describing the obstacles in the
+//   //  * level. See lighting-plugin/hull-from-tiles.
+//   //  *
+//   //  * @memberof NavMeshPlugin
+//   //  */
+//   // buildMesh(levelName, hulls) {
+//   //     const contours = this._buildContours(hulls);
+//   //     // Get an array of triangulated vertices
+//   //     const triangles = triangulate(contours, false); // Counter-clockwise ordering!
+//   //     const polygons = [];
+//   //     for (let i = 0; i < triangles.length; i += 6) {
+//   //         const poly = new Phaser.Polygon(
+//   //             // These should be in counter-clockwise order from triangulate
+//   //             triangles[i + 0], triangles[i + 1],
+//   //             triangles[i + 2], triangles[i + 3],
+//   //             triangles[i + 4], triangles[i + 5]
+//   //         );
+//   //         polygons.push(poly);
+//   //     }
+//   //     const navMesh = new NavMesh(this.game, polygons);
+//   //     this._navMeshes[levelName] = navMesh;
+//   //     this._currentNavMesh = navMesh;
+//   // }
+
+//   // /**
+//   //  * @param {[]} hulls
+//   //  * @returns
+//   //  *
+//   //  * @memberof NavMeshPlugin
+//   //  */
+//   // _buildContours(hulls) {
+//   //     const w = this.game.width;
+//   //     const h = this.game.height;
+//   //     // Start the contours
+//   //     const contours = [
+//   //         // Full screen - counter clockwise
+//   //         Float32Array.of(0,0, 0,h, w,h, w,0)
+//   //     ];
+//   //     // For each convex hull add the contour
+//   //     for (const hull of hulls) {
+//   //         const contour = [];
+//   //         for (const lineInfo of hull) {
+//   //             contour.push(lineInfo.line.start.x, lineInfo.line.start.y);
+//   //         }
+//   //         contours.push(Float32Array.from(contour));
+//   //     }
+//   //     return contours;
+//   // }
+// }
