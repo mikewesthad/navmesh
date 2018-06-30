afbf28f353ba098091718fcf1e4749ad94fa81f8
Michael Hadley <michaelwesthadley@gmail.com>
2018-06-30 17:26:34 -0500

    Update performance - but left updating phaser astar and easystart for another day...

--- packages/examples/src/performance/js/main.js
+++ packages/examples/src/performance/js/main.js
@@ -1,32 +1,29 @@
-// // Phaser 2 wasn't intended to be used with npm modules. See webpack config for how these libs are
-// // exposed. These need to be imported here so that all other files have access to them from the
-// // global scope.
-// import pixi from "pixi";
-// import p2 from "p2";
-// import Phaser from "phaser";
-
-// import load from "./states/load";
-// import start from "./states/start";
-
-// const game = new Phaser.Game({
-//   width: 750,
-//   height: 750,
-//   renderer: Phaser.WEBGL,
-//   parent: "game-container"
-// });
-
-// game.state.add("load", load);
-// game.state.add("start", start);
-
-// game.state.start("load");
-
 import Phaser from "phaser";
+import Load from "./scenes/load";
+import Start from "./scenes/start";
+
+// Import "phaser-navmesh" for transpiled code and "phaser-navmesh/src" for non-transpiled code
+import PhaserNavMesh from "phaser-navmesh/src";
 
 const game = new Phaser.Game({
   type: Phaser.AUTO,
   parent: "game-container",
   width: 750,
   height: 750,
-  backgroundColor: "#ff0000",
-  pixelArt: false
+  backgroundColor: "#fff",
+  pixelArt: false,
+  plugins: {
+    scene: [
+      {
+        key: "NavMeshPlugin", // Key to store the plugin class under in cache
+        plugin: PhaserNavMesh, // Class that constructs plugins
+        mapping: "navMeshPlugin", // Property mapping to use for the scene, e.g. this.navMeshPlugin
+        start: true
+      }
+    ]
+  }
 });
+
+game.scene.add("load", Load);
+game.scene.add("start", Start);
+game.scene.start("load");
---
+++ packages/examples/src/performance/js/scenes/start.js
@@ -0,0 +1,132 @@
+import Phaser from "phaser";
+// import "../plugins/phaser-astar";
+// import EasyStarPlugin from "../plugins/easy-star-plugin";
+
+export default class StartState extends Phaser.Scene {
+  create() {
+    // -- Setup --
+
+    const tilemap = this.add.tilemap("map");
+    this.tilemap = tilemap;
+    const wallTileset = tilemap.addTilesetImage("tiles", "tiles");
+    tilemap.createStaticLayer("bg", wallTileset);
+    const wallLayer = tilemap.createStaticLayer("walls", wallTileset);
+    wallLayer.setCollisionByProperty({ collides: true });
+    this.wallLayer = wallLayer;
+
+    // -- Plugins --
+
+    const objectLayer = tilemap.getObjectLayer("navmesh");
+    const navMesh = this.navMeshPlugin.buildMeshFromTiled("mesh1", objectLayer, 12.5);
+
+    // // Configure Phaser A* Plugin
+    // this.phaserAStar = this.game.plugins.add(Phaser.Plugin.AStar);
+    // this.phaserAStar.setAStarMap(tilemap, "walls", "tiles");
+
+    // // Configure Easy Star Plugin
+    // this.easyStar = this.game.plugins.add(EasyStarPlugin);
+    // this.easyStar.setGrid(wallLayer, [-1]);
+
+    // -- Compare Performance ---
+
+    const iterations = 1000;
+    const shortPaths = [];
+    const longPaths = [];
+    for (let i = 0; i < iterations; i += 1) {
+      shortPaths.push(this.getRandomPointsWithDistance(50, 150));
+      longPaths.push(this.getRandomPointsWithDistance(600));
+    }
+
+    let start;
+    const perf = { shortPaths: {}, longPaths: {} };
+
+    // start = performance.now();
+    // for (const path of shortPaths) this._phaserAStar(path[0], path[1]);
+    // perf.shortPaths.phaserAStar = (performance.now() - start) / iterations;
+
+    // start = performance.now();
+    // for (const path of shortPaths) this.easyStar.getWorldPath(path[0], path[1]);
+    // perf.shortPaths.easyStar = (performance.now() - start) / iterations;
+
+    start = performance.now();
+    for (const path of shortPaths) navMesh.findPath(path[0], path[1]);
+    perf.shortPaths.navMesh = (performance.now() - start) / iterations;
+
+    // start = performance.now();
+    // for (const path of longPaths) this._phaserAStar(path[0], path[1]);
+    // perf.longPaths.phaserAStar = (performance.now() - start) / iterations;
+
+    // start = performance.now();
+    // for (const path of longPaths) this.easyStar.getWorldPath(path[0], path[1]);
+    // perf.longPaths.easyStar = (performance.now() - start) / iterations;
+
+    start = performance.now();
+    for (const path of longPaths) navMesh.findPath(path[0], path[1]);
+    perf.longPaths.navMesh = (performance.now() - start) / iterations;
+
+    let message = `Performance Comparison, ${iterations} iterations, 30x30 tilemap`;
+    message += "\n\nShort paths (150 - 500 pixel length)\n";
+    // message += `\n\tAStart Plugin: ${perf.shortPaths.phaserAStar.toFixed(5)}ms`;
+    // message += `\n\tEasyStar Plugin: ${perf.shortPaths.easyStar.toFixed(5)}ms`;
+    message += `\n\tNavMesh Plugin: ${perf.shortPaths.navMesh.toFixed(5)}ms`;
+    // const shortNavVsEasy = perf.shortPaths.easyStar / perf.shortPaths.navMesh;
+    // const shortNavVsPhaser = perf.shortPaths.phaserAStar / perf.shortPaths.navMesh;
+    // message += `\n\tNavMesh is ${shortNavVsPhaser.toFixed(2)}x faster than Phaser AStar`;
+    // message += `\n\tNavMesh is ${shortNavVsEasy.toFixed(2)}x faster than EasyStar`;
+    message += "\n\nLong paths (600 pixels and greater length)\n";
+    // message += `\n\tAStart Plugin: ${perf.longPaths.phaserAStar.toFixed(5)}ms`;
+    // message += `\n\tEasyStar Plugin: ${perf.longPaths.easyStar.toFixed(5)}ms`;
+    message += `\n\tNavMesh Plugin: ${perf.longPaths.navMesh.toFixed(5)}ms`;
+    // const longNavVsEasy = perf.longPaths.easyStar / perf.longPaths.navMesh;
+    // const longNavVsPhaser = perf.longPaths.phaserAStar / perf.longPaths.navMesh;
+    // message += `\n\tNavMesh is ${longNavVsPhaser.toFixed(2)}x faster than Phaser AStar`;
+    // message += `\n\tNavMesh is ${longNavVsEasy.toFixed(2)}x faster than EasyStar`;
+
+    console.log(message);
+  }
+
+  _phaserAStar(start, end) {
+    const startTile = this.wallLayer.getTileXY(start.x, start.y, {});
+    const endTile = this.wallLayer.getTileXY(end.x, end.y, {});
+    return this.phaserAStar.findPath(startTile, endTile);
+  }
+
+  _drawPath(graphic, path) {
+    graphic.lineStyle(5, 0xffd900);
+    const p = new Phaser.Polygon(...path);
+    p.closed = false;
+    graphic.drawShape(p);
+    graphic.beginFill(0xffd900);
+    graphic.drawEllipse(path[0].x, path[0].y, 10, 10);
+    const lastPoint = path[path.length - 1];
+    graphic.drawEllipse(lastPoint.x, lastPoint.y, 10, 10);
+    graphic.endFill();
+  }
+
+  getRandomEmptyPoint() {
+    // Find random tile that is empty
+    let tileX, tileY, tile;
+    do {
+      tileX = Phaser.Math.Between(0, this.tilemap.width);
+      tileY = Phaser.Math.Between(0, this.tilemap.height);
+      tile = this.wallLayer.hasTileAt(tileX, tileY);
+    } while (tile && tile.collides);
+    // Convert from tile location to pixel location (at center of tile)
+    return new Phaser.Math.Vector2(
+      tileX * this.tilemap.tileWidth + this.tilemap.tileWidth / 2,
+      tileY * this.tilemap.tileHeight + this.tilemap.tileHeight / 2
+    );
+  }
+
+  getRandomPointsWithDistance(minDistance = 0, maxDistance = Number.MAX_VALUE) {
+    let p1 = this.getRandomEmptyPoint();
+    let p2 = this.getRandomEmptyPoint();
+    let d = p1.distance(p2);
+    while (d < minDistance || d > maxDistance) {
+      p1 = this.getRandomEmptyPoint();
+      p2 = this.getRandomEmptyPoint();
+      d = p1.distance(p2);
+    }
+    return [p1, p2];
+  }
+}
