c319b5d35d98fc18c83d7a664deead7a6eda1ec9
Michael Hadley <michaelwesthadley@gmail.com>
2021-02-09 21:09:12 -0600

    Update webpack to build ts files

--- packages/navmesh/webpack.config.js
+++ packages/navmesh/webpack.config.js
@@ -4,14 +4,14 @@ const path = require("path");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 const root = __dirname;
 
-module.exports = function(env, argv) {
+module.exports = function (env, argv) {
   const isDev = argv.mode === "development";
 
   return {
     context: path.join(root, "src"),
     entry: {
-      navmesh: "./index.js",
-      "navmesh.min": "./index.js"
+      navmesh: "./index.ts",
+      "navmesh.min": "./index.ts",
     },
     output: {
       filename: "[name].js",
@@ -19,20 +19,28 @@ module.exports = function(env, argv) {
       library: "NavMesh",
       libraryTarget: "umd",
       libraryExport: "default",
-      globalObject: '(typeof self !== "undefined" ? self : this)'
+      globalObject: '(typeof self !== "undefined" ? self : this)',
     },
     optimization: {
-      minimizer: [new UglifyJsPlugin({ include: /\.min\.js$/, sourceMap: true })]
+      minimizer: [new UglifyJsPlugin({ include: /\.min\.js$/, sourceMap: true })],
+    },
+    resolve: {
+      extensions: [".ts", ".js"],
     },
     module: {
       rules: [
         {
+          test: /\.ts$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
+        },
+        {
           test: /\.js$/,
           exclude: /node_modules/,
-          use: { loader: "babel-loader", options: { root: "../../" } }
-        }
-      ]
+          use: { loader: "babel-loader", options: { root: "../../" } },
+        },
+      ],
     },
-    devtool: isDev ? "eval-source-map" : "source-map"
+    devtool: isDev ? "eval-source-map" : "source-map",
   };
 };
