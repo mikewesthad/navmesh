14628e7a78e12bc3a49ed32b33478c853889c5ce
Michael Hadley <michaelwesthadley@gmail.com>
2021-02-10 20:21:32 -0600

    Update webpack configs

--- packages/phaser-navmesh/webpack.config.js
+++ packages/phaser-navmesh/webpack.config.js
@@ -4,42 +4,43 @@ const path = require("path");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 const root = __dirname;
 
-module.exports = function(env, argv) {
+module.exports = function (env, argv) {
   const isDev = argv.mode === "development";
 
   return {
+    mode: isDev ? "development" : "production",
     context: path.join(root, "src"),
     entry: {
       "phaser-navmesh": "./index.js",
-      "phaser-navmesh.min": "./index.js"
+      "phaser-navmesh.min": "./index.js",
     },
     output: {
       filename: "[name].js",
       path: path.resolve(root, "dist"),
       library: "PhaserNavMeshPlugin",
       libraryTarget: "umd",
-      libraryExport: "default"
+      libraryExport: "default",
     },
     optimization: {
-      minimizer: [new UglifyJsPlugin({ include: /\.min\.js$/, sourceMap: true })]
+      minimizer: [new UglifyJsPlugin({ include: /\.min\.js$/, sourceMap: true })],
     },
     externals: {
       phaser: {
         root: "Phaser",
         commonjs: "phaser",
         commonjs2: "phaser",
-        amd: "phaser"
-      }
+        amd: "phaser",
+      },
     },
     module: {
       rules: [
         {
           test: /\.js$/,
           exclude: /node_modules/,
-          use: { loader: "babel-loader", options: { root: "../../" } }
-        }
-      ]
+          use: { loader: "babel-loader", options: { root: "../../" } },
+        },
+      ],
     },
-    devtool: isDev ? "eval-source-map" : "source-map"
+    devtool: isDev ? "eval-source-map" : "source-map",
   };
 };
--- packages/phaser2-navmesh/webpack.config.js
+++ packages/phaser2-navmesh/webpack.config.js
@@ -4,37 +4,38 @@ const path = require("path");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 const root = __dirname;
 
-module.exports = function(env, argv) {
+module.exports = function (env, argv) {
   const isDev = argv.mode === "development";
 
   return {
+    mode: isDev ? "development" : "production",
     context: path.join(root, "src"),
     entry: {
       "phaser2-navmesh": "./index.js",
-      "phaser2-navmesh.min": "./index.js"
+      "phaser2-navmesh.min": "./index.js",
     },
     output: {
       filename: "[name].js",
       path: path.resolve(root, "dist"),
       library: "Phaser2NavMeshPlugin",
       libraryTarget: "umd",
-      libraryExport: "default"
+      libraryExport: "default",
     },
     optimization: {
-      minimizer: [new UglifyJsPlugin({ include: /\.min\.js$/, sourceMap: true })]
+      minimizer: [new UglifyJsPlugin({ include: /\.min\.js$/, sourceMap: true })],
     },
     externals: {
-      phaser: "Phaser"
+      phaser: "Phaser",
     },
     module: {
       rules: [
         {
           test: /\.js$/,
           exclude: /node_modules/,
-          use: { loader: "babel-loader", options: { root: "../../" } }
-        }
-      ]
+          use: { loader: "babel-loader", options: { root: "../../" } },
+        },
+      ],
     },
-    devtool: isDev ? "eval-source-map" : "source-map"
+    devtool: isDev ? "eval-source-map" : "source-map",
   };
 };
