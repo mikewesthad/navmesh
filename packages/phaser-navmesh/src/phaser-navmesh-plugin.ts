2e0d4153567e17829458cc45cce7c74485cb7c1d
Michael Hadley <michaelwesthadley@gmail.com>
2021-02-11 08:34:55 -0600

    phaser-navmesh: first pass at converting to TS

---
+++ packages/phaser-navmesh/src/phaser-navmesh-plugin.ts
@@ -0,0 +1,99 @@
+import Phaser from "phaser";
+import PhaserNavMesh from "./phaser-navmesh";
+
+/**
+ * This class can create navigation meshes for use in Phaser 3. The navmeshes can be constructed
+ * from convex polygons embedded in a Tiled map. The class that conforms to Phaser 3's plugin
+ * structure.
+ *
+ * @export
+ * @class PhaserNavMeshPlugin
+ */
+export default class PhaserNavMeshPlugin extends Phaser.Plugins.ScenePlugin {
+  private phaserNavMeshes: Record<string, PhaserNavMesh> = {};
+
+  public constructor(scene: Phaser.Scene, pluginManager: Phaser.Plugins.PluginManager) {
+    super(scene, pluginManager);
+    if (!scene.sys.settings.isBooted) this.systems.events.once("boot", this.boot, this);
+  }
+
+  /** Phaser.Scene lifecycle event */
+  public boot() {
+    const emitter = this.systems.events;
+    emitter.once("destroy", this.destroy, this);
+  }
+
+  /** Phaser.Scene lifecycle event - noop in this plugin, but still required. */
+  public init() {}
+
+  /** Phaser.Scene lifecycle event - noop in this plugin, but still required.*/
+  public start() {}
+
+  /** Phaser.Scene lifecycle event - will destroy all navmeshes created. */
+  public destroy() {
+    this.systems.events.off("boot", this.boot, this);
+    const meshes = Object.values(this.phaserNavMeshes);
+    this.phaserNavMeshes = {};
+    meshes.forEach((m) => m.destroy());
+  }
+
+  /**
+   * Remove the navmesh stored under the given key from the plugin. This does not destroy the
+   * navmesh.
+   * @param key
+   */
+  public removeMesh(key: string) {
+    if (this.phaserNavMeshes[key]) delete this.phaserNavMeshes[key];
+  }
+
+  /**
+   * Load a navmesh from Tiled. Currently assumes that the polygons are squares! Does not support
+   * tilemap layer scaling, rotation or position.
+   * @param key Key to use when storing this navmesh within the plugin.
+   * @param objectLayer The ObjectLayer from a tilemap that contains the polygons that make up the
+   * navmesh.
+   * @param meshShrinkAmount The amount (in pixels) that the navmesh has been shrunk around
+   * obstacles (a.k.a the amount obstacles have been expanded)
+   */
+  public buildMeshFromTiled(
+    key: string,
+    objectLayer: Phaser.Tilemaps.ObjectLayer,
+    meshShrinkAmount = 0
+  ) {
+    if (this.phaserNavMeshes[key]) {
+      console.warn(`NavMeshPlugin: a navmesh already exists with the given key: ${key}`);
+      return this.phaserNavMeshes[key];
+    }
+
+    if (!objectLayer || objectLayer.objects.length === 0) {
+      console.warn(
+        `NavMeshPlugin: The given tilemap object layer is empty or undefined: ${objectLayer}`
+      );
+    }
+
+    const objects = objectLayer.objects ?? [];
+
+    // Loop over the objects and construct a polygon - assumes a rectangle for now!
+    // TODO: support layer position, scale, rotation
+    const polygons = objects.map((obj) => {
+      const h = obj.height ?? 0;
+      const w = obj.width ?? 0;
+      const left = obj.x ?? 0;
+      const top = obj.y ?? 0;
+      const bottom = top + h;
+      const right = left + w;
+      return [
+        { x: left, y: top },
+        { x: left, y: bottom },
+        { x: right, y: bottom },
+        { x: right, y: top },
+      ];
+    });
+
+    const mesh = new PhaserNavMesh(this, this.scene, key, polygons, meshShrinkAmount);
+
+    this.phaserNavMeshes[key] = mesh;
+
+    return mesh;
+  }
+}
---
+++ packages/phaser-navmesh/src/phaser-navmesh.ts
@@ -0,0 +1,182 @@
+import NavMesh, { Point, PolyPoints } from "navmesh"; // import the source - not the dist - no need to double build
+import Phaser from "phaser";
+import PhaserNavMeshPlugin from "./phaser-navmesh-plugin";
+
+/**
+ * A wrapper around {@link NavMesh} for Phaser 3. Create instances of this class from
+ * {@link PhaserNavMeshPlugin}. This is the workhorse that represents a navigation mesh built from a
+ * series of polygons. Once built, the mesh can be asked for a path from one point to another point.
+ *
+ * Compared to {@link NavMesh}, this adds visual debugging capabilities and converts paths to
+ * Phaser-compatible point instances.
+ *
+ * @export
+ * @class PhaserNavMesh
+ */
+export default class PhaserNavMesh {
+  private key: string;
+  private plugin: PhaserNavMeshPlugin;
+  private scene: Phaser.Scene;
+  private debugGraphics: Phaser.GameObjects.Graphics | null;
+  private navMesh: NavMesh;
+
+  /**
+   * Creates an instance of PhaserNavMesh.
+   * @param plugin The plugin that owns this mesh.
+   * @param scene
+   * @param key The key the mesh is stored under within the plugin.
+   * @param meshPolygonPoints Array where each element is an array of point-like objects that
+   * defines a polygon.
+   * @param meshShrinkAmount The amount (in pixels) that the navmesh has been shrunk around
+   * obstacles (a.k.a the amount obstacles have been expanded)
+   */
+  public constructor(
+    plugin: PhaserNavMeshPlugin,
+    scene: Phaser.Scene,
+    key: string,
+    meshPolygonPoints: PolyPoints[],
+    meshShrinkAmount = 0
+  ) {
+    this.key = key;
+    this.plugin = plugin;
+    this.scene = scene;
+    this.debugGraphics = null;
+    this.navMesh = new NavMesh(meshPolygonPoints, meshShrinkAmount);
+  }
+
+  /**
+   * See {@link NavMesh#findPath}. This implements the same functionality, except that the returned
+   * path is converted to Phaser-compatible points.
+   * @param startPoint A point-like object
+   * @param endPoint A point-like object
+   * @param PointClass The class used to represent points in the final path
+   * @returns An array of points if a path is found, or null if no path
+   */
+  public findPath(startPoint: Point, endPoint: Point, PointClass = Phaser.Geom.Point) {
+    const path = this.navMesh.findPath(startPoint, endPoint);
+    return path ? path.map(({ x, y }) => new PointClass(x, y)) : path;
+  }
+
+  /**
+   * Enable the debug drawing graphics. If no graphics object is provided, a new instance will be
+   * created.
+   * @param graphics An optional graphics object for the mesh to use for debug drawing. Note, the
+   * mesh will destroy this graphics object when the mesh is destroyed.
+   * @returns The graphics object this mesh uses.
+   */
+  public enableDebug(graphics: Phaser.GameObjects.Graphics) {
+    if (!graphics && !this.debugGraphics) {
+      this.debugGraphics = this.scene.add.graphics();
+    } else if (graphics) {
+      if (this.debugGraphics) this.debugGraphics.destroy();
+      this.debugGraphics = graphics;
+    }
+
+    this.debugGraphics!.visible = true;
+
+    return this.debugGraphics;
+  }
+
+  /** Hide the debug graphics, but don't destroy it. */
+  public disableDebug() {
+    if (this.debugGraphics) this.debugGraphics.visible = false;
+  }
+
+  /** Returns true if the debug graphics object is enabled and visible. */
+  public isDebugEnabled() {
+    return this.debugGraphics && this.debugGraphics.visible;
+  }
+
+  /** Clear the debug graphics. */
+  public debugDrawClear() {
+    if (this.debugGraphics) this.debugGraphics.clear();
+  }
+
+  /**
+   * Visualize the polygons in the navmesh by drawing them to the debug graphics.
+   * @param options
+   * @param [options.drawCentroid=true] For each polygon, show the approx centroid
+   * @param [options.drawBounds=false] For each polygon, show the bounding radius
+   * @param [options.drawNeighbors=true] For each polygon, show the connections to neighbors
+   * @param [options.drawPortals=true] For each polygon, show the portal edges
+   * @param [options.palette=[0x00a0b0, 0x6a4a3c, 0xcc333f, 0xeb6841, 0xedc951]] An array of
+   * Phaser-compatible format colors to use when drawing the individual polygons. The first poly
+   * uses the first color, the second poly uses the second color, etc.
+   */
+  public debugDrawMesh({
+    drawCentroid = true,
+    drawBounds = false,
+    drawNeighbors = true,
+    drawPortals = true,
+    palette = [0x00a0b0, 0x6a4a3c, 0xcc333f, 0xeb6841, 0xedc951],
+  } = {}) {
+    if (!this.debugGraphics) return;
+
+    const graphics = this.debugGraphics;
+    const navPolys = this.navMesh.getPolygons();
+
+    navPolys.forEach((poly) => {
+      const color = palette[poly.id % palette.length];
+      graphics.fillStyle(color);
+      graphics.fillPoints(poly.getPoints(), true);
+
+      if (drawCentroid) {
+        graphics.fillStyle(0x000000);
+        graphics.fillCircle(poly.centroid.x, poly.centroid.y, 4);
+      }
+
+      if (drawBounds) {
+        graphics.lineStyle(1, 0xffffff);
+        graphics.strokeCircle(poly.centroid.x, poly.centroid.y, poly.boundingRadius);
+      }
+
+      if (drawNeighbors) {
+        graphics.lineStyle(2, 0x000000);
+        poly.neighbors.forEach((n) => {
+          graphics.lineBetween(poly.centroid.x, poly.centroid.y, n.centroid.x, n.centroid.y);
+        });
+      }
+
+      if (drawPortals) {
+        graphics.lineStyle(10, 0x000000);
+        poly.portals.forEach((portal) =>
+          graphics.lineBetween(portal.start.x, portal.start.y, portal.end.x, portal.end.y)
+        );
+      }
+    });
+  }
+
+  /**
+   * Visualize a path (array of points) on the debug graphics.
+   * @param path Array of point-like objects in the form {x, y}
+   * @param color
+   * @param thickness
+   * @param alpha
+   */
+  public debugDrawPath(path: Point[], color = 0x00ff00, thickness = 10, alpha = 1) {
+    if (!this.debugGraphics) return;
+
+    if (path && path.length) {
+      // Draw line for path
+      this.debugGraphics.lineStyle(thickness, color, alpha);
+      this.debugGraphics.strokePoints(path);
+
+      // Draw circle at start and end of path
+      this.debugGraphics.fillStyle(color, alpha);
+      const d = 1.2 * thickness;
+      this.debugGraphics.fillCircle(path[0].x, path[0].y, d);
+
+      if (path.length > 1) {
+        const lastPoint = path[path.length - 1];
+        this.debugGraphics.fillCircle(lastPoint.x, lastPoint.y, d);
+      }
+    }
+  }
+
+  /** Destroy the mesh, kill the debug graphic and unregister itself with the plugin. */
+  public destroy() {
+    if (this.navMesh) this.navMesh.destroy();
+    if (this.debugGraphics) this.debugGraphics.destroy();
+    this.plugin.removeMesh(this.key);
+  }
+}
