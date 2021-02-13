95a80e251a4d3b1a571794e27be079a5ebf6d39f
Michael Hadley <michaelwesthadley@gmail.com>
2021-02-12 20:44:56 -0600

    Update phaser-navmesh webpack to cut unnecessary bits

--- packages/phaser-navmesh/webpack.config.js
+++ packages/phaser-navmesh/webpack.config.js
@@ -1,28 +1,20 @@
 /* eslint-env node */
 
 const path = require("path");
-const TerserPlugin = require("terser-webpack-plugin");
 const root = __dirname;
 
 module.exports = function (env, argv) {
   const isDev = argv.mode === "development";
 
   return {
-    mode: isDev ? "development" : "production",
     context: path.join(root, "src"),
-    entry: {
-      "phaser-navmesh-plugin": "./index.ts",
-      "phaser-navmesh-plugin.min": "./index.ts",
-    },
+    entry: "./index.ts",
     output: {
-      filename: "[name].js",
+      filename: "phaser-navmesh-plugin.js",
       path: path.resolve(root, "dist"),
       library: "PhaserNavMeshPlugin",
       libraryTarget: "umd",
       libraryExport: "default",
-    },
-    optimization: {
-      minimizer: [new TerserPlugin({ include: /\.min\.js$/ })],
     },
     externals: {
       phaser: {
