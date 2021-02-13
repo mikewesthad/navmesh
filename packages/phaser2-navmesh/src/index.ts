9088b21728929fe52605eaf71a8604ad86b05c96
Michael Hadley <michaelwesthadley@gmail.com>
2021-02-12 21:49:57 -0600

    Rm testing log

--- packages/phaser2-navmesh/src/index.ts
+++ packages/phaser2-navmesh/src/index.ts
@@ -1,6 +1,5 @@
 import Phaser2NavMeshPlugin from "./phaser2-navmesh-plugin";
 import Phaser2NavMesh from "./phaser2-navmesh";
 
-console.log("hey");
 export default Phaser2NavMeshPlugin;
 export { Phaser2NavMesh };
