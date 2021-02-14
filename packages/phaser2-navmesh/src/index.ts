431af1f6b649ec85cbe3be709f2d48d887d27e96
Michael Hadley <michaelwesthadley@gmail.com>
2021-02-13 21:10:58 -0600

    Add TS reference to phaser-ce

--- packages/phaser2-navmesh/src/index.ts
+++ packages/phaser2-navmesh/src/index.ts
@@ -1,3 +1,5 @@
+/// <reference path="../../../node_modules/phaser-ce/typescript/phaser.comments.d.ts" />
+
 import Phaser2NavMeshPlugin from "./phaser2-navmesh-plugin";
 import Phaser2NavMesh from "./phaser2-navmesh";
 
