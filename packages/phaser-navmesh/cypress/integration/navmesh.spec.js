b983caab74c05f6b2a2d598eb1481a001f9cb9d6
Michael Hadley <michaelwesthadley@gmail.com>
2021-02-27 18:55:55 -0600

    phaser-navmesh: add sample cypress test

---
+++ packages/phaser-navmesh/cypress/integration/navmesh.spec.js
@@ -0,0 +1,25 @@
+import Phaser from "phaser";
+import NavMeshPlugin from "phaser-navmesh";
+
+describe("NavMeshPlugin", () => {
+  it("is installed properly as a scene plugin", () => {
+    const game = new Phaser.Game({
+      plugins: {
+        scene: [
+          {
+            key: "NavMeshPlugin",
+            plugin: NavMeshPlugin,
+            mapping: "navMeshPlugin",
+            start: true,
+          },
+        ],
+      },
+    });
+    const scene = {
+      create() {
+        expect(this.navMeshPlugin).to.be.instanceOf(NavMeshPlugin);
+      },
+    };
+    game.scene.add("main", scene, true);
+  });
+});
