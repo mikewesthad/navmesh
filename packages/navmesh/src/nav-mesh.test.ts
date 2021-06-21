9b539336eac852aa1e7190571d16ffd670fc54ad
Mike Hadley <michaelwesthadley@gmail.com>
2021-06-21 16:55:23 -0500

    nav-mesh: use optional chaining for possible null results in tests

--- packages/navmesh/src/nav-mesh.test.ts
+++ packages/navmesh/src/nav-mesh.test.ts
@@ -157,29 +157,29 @@ describe("findClosestMeshPoint", () => {
 
   it("should return poly 1 for point inside poly 1", () => {
     const result = navMesh.findClosestMeshPoint(v2(5, 5));
-    expect(result.polygon.id).toBe(0);
-    expect(result.point).toEqual({ x: 5, y: 5 });
+    expect(result?.polygon?.id).toBe(0);
+    expect(result?.point).toEqual({ x: 5, y: 5 });
   });
 
   it("should return poly 1 or 2 for point on shared edge between poly 1 and 2", () => {
     const result = navMesh.findClosestMeshPoint(v2(10, 5));
-    expect(result.polygon.id).toBeGreaterThanOrEqual(0);
-    expect(result.polygon.id).toBeLessThanOrEqual(1);
+    expect(result?.polygon?.id).toBeGreaterThanOrEqual(0);
+    expect(result?.polygon?.id).toBeLessThanOrEqual(1);
   });
 
   it("should return top left corner of poly 1 for a point just outside of top left corner", () => {
     const result = navMesh.findClosestMeshPoint(v2(-10, -10));
-    expect(result.point).toEqual({ x: 0, y: 0 });
+    expect(result?.point).toEqual({ x: 0, y: 0 });
   });
 
   it("should return top middle of poly 1 for a point just outside of top middle", () => {
     const result = navMesh.findClosestMeshPoint(v2(-10, 5));
-    expect(result.point).toEqual({ x: 0, y: 5 });
+    expect(result?.point).toEqual({ x: 0, y: 5 });
   });
 
   it("should return poly 2 or 4 for a point equidistant from them", () => {
     const result = navMesh.findClosestMeshPoint(v2(25, 5));
-    const isPoly2or4 = result.polygon.id === 1 || result.polygon.id === 3;
+    const isPoly2or4 = result?.polygon?.id === 1 || result?.polygon?.id === 3;
     console.log(result.point, result.distance);
     expect(isPoly2or4).toBe(true);
   });
